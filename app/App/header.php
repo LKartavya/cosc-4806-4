989a291dfd3ed7a3d98704ecd94043f2fd4cd14d
LKartavya <147118099+LKartavya@users.noreply.github.com>
2025-07-05 03:32:05 +0000

    update authenticate php

--- app/App/header.php
+++
@@ -1,1 +0,0 @@
-<li><a href="/notes">Reminders</a></li>
--- app/core/App.php
+++ app/core/App.php
@@ -21,7 +21,7 @@ class App {
          * if not, then go to this->controller which is defaulted to home 
          */
 
-        if (isset($url[1]) && file_exists('app/controllers/' . $url[1] . '.php')) {
+        if (file_exists('app/controllers/' . $url[1] . '.php')) {
             $this->controller = $url[1];
 
             $_SESSION['controller'] = $this->controller;
@@ -64,8 +64,8 @@ class App {
         $u = "{$_SERVER['REQUEST_URI']}";
         //trims the trailing forward slash (rtrim), sanitizes URL, explode it by forward slash to get elements
         $url = explode('/', filter_var(rtrim($u, '/'), FILTER_SANITIZE_URL));
-		unset($url[0]);
-		return $url;
+        unset($url[0]);
+        return $url;
     }
 
 }
